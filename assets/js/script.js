dcec99e2adfbf0fdaa5416cb470e1d0119dda61a
Saurbh Moynak <94280322+Saurbhmoynak@users.noreply.github.com>
2023-05-02 16:12:05 +0530

    Changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-
-
 // form repeater
 $(document).ready(function(){
     $('.repeater').repeater({
@@ -18,4 +16,24 @@ $(document).ready(function(){
         },
         isFirstItemUndeletable: true
     })
+})
+
+
+function switchForm(className, e) {
+	e.preventDefault();
+	const allForm = document.querySelectorAll('form');
+	const form = document.querySelector(`form.${className}`);
+
+	allForm.forEach(item=> {
+		item.classList.remove('active');
+	})
+	form.classList.add('active');
+}
+
+
+const registerPassword = document.querySelector('form.register #password');
+const registerConfirmPassword = document.querySelector('form.register #confirm-pass');
+
+registerPassword.addEventListener('input', function () {
+	registerConfirmPassword.pattern = `${this.value}`;
 })
